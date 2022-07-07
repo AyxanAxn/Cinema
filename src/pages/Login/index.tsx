a35ac1d46775fbc118cf62437c851299f6533900
Emil Sadigov <emilsadiqov2001@gmail.com>
2022-07-07 17:04:25 +0400

    feat: add styling

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
 
+import "./style.css";
+
 export interface IDetails {
     name: string;
     email: string;
@@ -24,6 +26,9 @@ export const Login = () => {
                 email: details.email,
                 password: details.password
             });
+            setError("");
+            console.log("success");
+            
         }
         else{
             setError("Details do not match");
@@ -38,24 +43,22 @@ export const Login = () => {
     };
 
     return (
-        <form onSubmit={submitHandler}>
-            <div className='form-inner'>
-                <h2>Login</h2>
-                {(error != "") ? (<div className='error'>{error}</div>) : ""}
-                <div className='form-group'>
-                    <label htmlFor='name'>Name: </label>
-                    <input type="text" name="name" id = "name" onChange={e => setDetails({...details, name: e.target.value})} value={details.name} />
-                </div>                
-                <div className='form-group'>
-                    <label htmlFor='email'>Email: </label>
-                    <input type="email" name="email" id = "email"  onChange={e => setDetails({...details, email: e.target.value})} value={details.email}/>
-                </div>
-                <div className='form-group'>
-                    <label htmlFor='password'>Password: </label>
-                    <input type="password" name="password" id = "password"  onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
-                </div>
-                <input type="submit" value="LOGIN" />
-            </div> 
+        <form className="main" onSubmit={submitHandler}>
+            <h2>Login</h2>
+            <div className='input-container'>
+                <label htmlFor='name'>Name: </label>
+                <input type="text" name="name" id = "name" onChange={e => setDetails({...details, name: e.target.value})} value={details.name} />
+            </div>                
+            <div className='input-container'>
+                <label htmlFor='email'>Email: </label>
+                <input type="email" name="email" id = "email"  onChange={e => setDetails({...details, email: e.target.value})} value={details.email}/>
+            </div>
+            <div className='input-container'>
+                <label htmlFor='password'>Password: </label>
+                <input type="password" name="password" id = "password"  onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
+            </div>
+            {(error != "") ? (<div className='error'>{error}</div>) : ""}
+            <input type="submit" className="button" value="LOGIN" />
         </form>
     );
 };
