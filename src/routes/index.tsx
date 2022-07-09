6fa37c7f4fc1be37ff66c67d33022ae79779f52b
Ayxan <User@DESKTOP-9O75NSO>
2022-07-09 23:26:11 +0400

    Added register page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,12 +5,14 @@ import {
 } from "react-router-dom";
 import { Login } from "../pages/Login";
 import { MainPage } from "../pages/MainPage";
+import { Register } from "../pages/Register";
 
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<MainPage />}/>
-            <Route path="/login" element={<Login />}/>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
         </Routes>
-    ); 
+    );
 };
