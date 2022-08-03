c4507098d9b1d8f3c33e86536c0b5f667b3ba48d
Emil Guluzade <58591330+EmilGlz@users.noreply.github.com>
2022-08-03 04:43:25 +0400

    Added navbar with routing in basic design (#8)
    * Added navbar with routing in basic design

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { AppRoutes } from "./routes";
-
+import { Nav } from "./components/Navigation";
 import { HTTP } from "./core/services/http";
 
 HTTP.createClient("api");
 
-
 function App() {
     return (
-        <AppRoutes />
+        <>
+            <Nav />
+            <AppRoutes />
+        </>
     );
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,5 @@
 import React from "react";
-import {
-    Routes,
-    Route,
-} from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { Login } from "../pages/Login";
 import { MainPage } from "../pages/MainPage";
 import { Register } from "../pages/Register";
@@ -10,9 +7,9 @@ import { Register } from "../pages/Register";
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+            <Route path='/' element={<MainPage />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
         </Routes>
     );
 };
